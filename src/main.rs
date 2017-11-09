837015b68cec09189977294f3203b4bb0cd4e81d
Osspial <osspial@gmail.com>
2017-11-08 19:56:04 -0500

    Make everything work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate harfbuzz_sys;
 extern crate freetype;
 extern crate libc;
+#[macro_use]
+extern crate lazy_static;
 
 use harfbuzz_sys::*;
 use freetype::freetype::*;
@@ -37,18 +39,16 @@ fn main() {
 
         let hb_font = hb_font_create(hb_face);
 
-        // let funcs = hb_font_funcs_create();
-        // hb_font_funcs_set_font_h_extents_func(funcs, Some(get_font_h_extents), ptr::null_mut(), None);
-        // hb_font_funcs_set_nominal_glyph_func(funcs, Some(get_font_nominal_glyph), ptr::null_mut(), None);
-        // hb_font_funcs_set_variation_glyph_func(funcs, Some(get_variation_glyph), ptr::null_mut(), None);
-        // hb_font_funcs_set_glyph_h_advance_func(funcs, None, ptr::null_mut(), None);
-        // hb_font_funcs_set_glyph_v_advance_func(funcs, None, ptr::null_mut(), None);
-        // hb_font_funcs_set_glyph_v_origin_func(funcs, None, ptr::null_mut(), None);
-        // hb_font_funcs_set_glyph_h_kerning_func(funcs, None, ptr::null_mut(), None);
-        // hb_font_funcs_set_glyph_extents_func(funcs, None, ptr::null_mut(), None);
-        // hb_font_funcs_set_glyph_contour_point_func(funcs, None, ptr::null_mut(), None);
-        // hb_font_funcs_set_glyph_name_func(funcs, None, ptr::null_mut(), None);
-        // hb_font_funcs_set_glyph_from_name_func(funcs, None, ptr::null_mut(), None);
+        hb_font_set_funcs(
+            hb_font,
+            HB_FUNCS.0,
+            Box::into_raw(Box::new(FontFuncData {
+                ft_face,
+                load_flags: (FT_LOAD_DEFAULT | FT_LOAD_NO_HINTING) as c_int,
+                symbol: (*ft_face).charmap != ptr::null_mut() && (*(*ft_face).charmap).encoding == FT_Encoding__FT_ENCODING_MS_SYMBOL
+            })) as *mut c_void,
+            Some(drop_font_func_data)
+        );
 
         hb_font_set_scale(hb_font,
             ((size.metrics.x_scale as u64 * (*ft_face).units_per_EM as u64 + (1<<15)) >> 16) as i32,
@@ -74,6 +74,34 @@ fn main() {
     }
 }
 
+struct FontFuncsWrapper(*mut hb_font_funcs_t);
+unsafe impl ::std::marker::Sync for FontFuncsWrapper {}
+impl Drop for FontFuncsWrapper {
+    fn drop(&mut self) {
+        unsafe{ hb_font_funcs_destroy(self.0) };
+    }
+}
+
+lazy_static!{
+    static ref HB_FUNCS: FontFuncsWrapper = unsafe {
+        let funcs = hb_font_funcs_create();
+        hb_font_funcs_set_font_h_extents_func(funcs, Some(get_font_h_extents), ptr::null_mut(), None);
+        hb_font_funcs_set_nominal_glyph_func(funcs, Some(get_font_nominal_glyph), ptr::null_mut(), None);
+        hb_font_funcs_set_variation_glyph_func(funcs, Some(get_variation_glyph), ptr::null_mut(), None);
+        hb_font_funcs_set_glyph_h_advance_func(funcs, Some(get_h_advance), ptr::null_mut(), None);
+        hb_font_funcs_set_glyph_v_advance_func(funcs, Some(get_v_advance), ptr::null_mut(), None);
+        hb_font_funcs_set_glyph_v_origin_func(funcs, Some(get_v_origin), ptr::null_mut(), None);
+        hb_font_funcs_set_glyph_h_kerning_func(funcs, Some(get_h_kerning), ptr::null_mut(), None);
+        hb_font_funcs_set_glyph_extents_func(funcs, Some(get_extents), ptr::null_mut(), None);
+        hb_font_funcs_set_glyph_contour_point_func(funcs, Some(get_contour_point), ptr::null_mut(), None);
+        hb_font_funcs_set_glyph_name_func(funcs, Some(get_glyph_name), ptr::null_mut(), None);
+        // hb_font_funcs_set_glyph_from_name_func(funcs, None, ptr::null_mut(), None);
+
+        hb_font_funcs_make_immutable(funcs);
+        FontFuncsWrapper(funcs)
+    };
+}
+
 struct FontFuncData {
     ft_face: FT_Face,
     load_flags: c_int,
@@ -86,7 +114,13 @@ unsafe extern "C" fn drop_font_func_data(ffd: *mut c_void) {
 
 // These functions are pretty much a direct Rust translation of hb-ft.cc's functions
 
-unsafe extern "C" fn get_font_h_extents(_: *mut hb_font_t, font_data: *mut c_void, metrics: *mut hb_font_extents_t, _: *mut c_void) -> hb_bool_t {
+unsafe extern "C" fn get_font_h_extents(
+    _: *mut hb_font_t,
+    font_data: *mut c_void,
+    metrics: *mut hb_font_extents_t,
+    _: *mut c_void
+) -> hb_bool_t
+{
     let ffd = &*(font_data as *const FontFuncData);
 
     let ft_metrics = &(*(*ffd.ft_face).size).metrics;
@@ -104,7 +138,14 @@ unsafe extern "C" fn get_font_h_extents(_: *mut hb_font_t, font_data: *mut c_voi
     1
 }
 
-unsafe extern "C" fn get_font_nominal_glyph(_: *mut hb_font_t, font_data: *mut c_void, unicode: hb_codepoint_t, glyph: *mut hb_codepoint_t, _: *mut c_void) -> hb_bool_t {
+unsafe extern "C" fn get_font_nominal_glyph(
+    _: *mut hb_font_t,
+    font_data: *mut c_void,
+    unicode: hb_codepoint_t,
+    glyph: *mut hb_codepoint_t,
+    _: *mut c_void
+) -> hb_bool_t
+{
     let ffd = &*(font_data as *const FontFuncData);
     let mut char_index = FT_Get_Char_Index(ffd.ft_face, unicode);
 
@@ -120,7 +161,14 @@ unsafe extern "C" fn get_font_nominal_glyph(_: *mut hb_font_t, font_data: *mut c
     1
 }
 
-unsafe extern "C" fn get_variation_glyph(_: *mut hb_font_t, font_data: *mut c_void, unicode: hb_codepoint_t, variation_selector: hb_codepoint_t, glyph: *mut hb_codepoint_t, _: *mut c_void) -> hb_bool_t {
+unsafe extern "C" fn get_variation_glyph(
+    _: *mut hb_font_t,
+    font_data: *mut c_void,
+    unicode: hb_codepoint_t,
+    variation_selector: hb_codepoint_t,
+    glyph: *mut hb_codepoint_t, _: *mut c_void
+) -> hb_bool_t
+{
     let ffd = &*(font_data as *const FontFuncData);
     let char_index = FT_Face_GetCharVariantIndex(ffd.ft_face, unicode, variation_selector);
 
@@ -133,7 +181,13 @@ unsafe extern "C" fn get_variation_glyph(_: *mut hb_font_t, font_data: *mut c_vo
     }
 }
 
-unsafe extern "C" fn get_h_advance(_: *mut hb_font_t, font_data: *mut c_void, glyph: hb_codepoint_t, _: *mut c_void) -> hb_position_t {
+unsafe extern "C" fn get_h_advance(
+    _: *mut hb_font_t,
+    font_data: *mut c_void,
+    glyph: hb_codepoint_t,
+    _: *mut c_void
+) -> hb_position_t
+{
     let ffd = &*(font_data as *const FontFuncData);
 
     let mut advance = 0;
@@ -149,7 +203,12 @@ unsafe extern "C" fn get_h_advance(_: *mut hb_font_t, font_data: *mut c_void, gl
     }
 }
 
-unsafe extern "C" fn get_v_advance(_: *mut hb_font_t, font_data: *mut c_void, glyph: hb_codepoint_t, _: *mut c_void) -> hb_position_t {
+unsafe extern "C" fn get_v_advance(
+    _: *mut hb_font_t,
+    font_data: *mut c_void,
+    glyph: hb_codepoint_t,
+    _: *mut c_void
+) -> hb_position_t {
     let ffd = &*(font_data as *const FontFuncData);
 
     let mut advance = 0;
@@ -165,7 +224,15 @@ unsafe extern "C" fn get_v_advance(_: *mut hb_font_t, font_data: *mut c_void, gl
     }
 }
 
-unsafe extern "C" fn get_glyph_v_origin(_: *mut hb_font_t, font_data: *mut c_void, glyph: hb_codepoint_t, x: *mut hb_position_t, y: *mut hb_position_t, _: *mut c_void) -> hb_bool_t {
+unsafe extern "C" fn get_v_origin(
+    _: *mut hb_font_t,
+    font_data: *mut c_void,
+    glyph: hb_codepoint_t,
+    x: *mut hb_position_t,
+    y: *mut hb_position_t,
+    _: *mut c_void
+) -> hb_bool_t
+{
     let ffd = &*(font_data as *const FontFuncData);
 
     match FT_Load_Glyph(ffd.ft_face, glyph, ffd.load_flags) {
@@ -187,7 +254,14 @@ unsafe extern "C" fn get_glyph_v_origin(_: *mut hb_font_t, font_data: *mut c_voi
     }
 }
 
-unsafe extern "C" fn get_glyph_h_kerning(_: *mut hb_font_t, font_data: *mut c_void, left_glyph: hb_codepoint_t, right_glyph: hb_codepoint_t, _: *mut c_void) -> hb_position_t {
+unsafe extern "C" fn get_h_kerning(
+    _: *mut hb_font_t,
+    font_data: *mut c_void,
+    left_glyph: hb_codepoint_t,
+    right_glyph: hb_codepoint_t,
+    _: *mut c_void
+) -> hb_position_t
+{
     let ffd = &*(font_data as *const FontFuncData);
 
     let mut kerningv = mem::uninitialized();
@@ -201,7 +275,14 @@ unsafe extern "C" fn get_glyph_h_kerning(_: *mut hb_font_t, font_data: *mut c_vo
     }
 }
 
-unsafe extern "C" fn get_glyph_extents(_: *mut hb_font_t, font_data: *mut c_void, glyph: hb_codepoint_t, extents: *mut hb_glyph_extents_t, _: *mut c_void) -> hb_bool_t {
+unsafe extern "C" fn get_extents(
+    _: *mut hb_font_t,
+    font_data: *mut c_void,
+    glyph: hb_codepoint_t,
+    extents: *mut hb_glyph_extents_t,
+    _: *mut c_void
+) -> hb_bool_t
+{
     let ffd = &*(font_data as *const FontFuncData);
     let extents = &mut *extents;
 
@@ -226,6 +307,54 @@ unsafe extern "C" fn get_glyph_extents(_: *mut hb_font_t, font_data: *mut c_void
         },
         _ => 0
     }
+}
+
+unsafe extern "C" fn get_contour_point(
+    _: *mut hb_font_t,
+    font_data: *mut ::libc::c_void,
+    glyph: hb_codepoint_t,
+    point_index: ::libc::c_uint,
+    x: *mut hb_position_t,
+    y: *mut hb_position_t,
+    _: *mut ::libc::c_void
+) -> hb_bool_t
+{
+    let ffd = &*(font_data as *const FontFuncData);
+
+    if FT_Error(0) != FT_Load_Glyph(ffd.ft_face, glyph, ffd.load_flags) {
+        return 0;
+    }
+
+    let glyph = &*(*ffd.ft_face).glyph;
+    if glyph.format != FT_Glyph_Format__FT_GLYPH_FORMAT_OUTLINE {
+        return 0;
+    }
+    if point_index as i64 >= glyph.outline.n_points as i64 {
+        return 0;
+    }
+
+    *x = (*glyph.outline.points.offset(point_index as isize)).x;
+    *y = (*glyph.outline.points.offset(point_index as isize)).y;
+
+    1
+}
+
+unsafe extern "C" fn get_glyph_name(
+    _: *mut hb_font_t,
+    font_data: *mut ::libc::c_void,
+    glyph: hb_codepoint_t,
+    name: *mut ::libc::c_char,
+    size: ::libc::c_uint,
+    _: *mut ::libc::c_void
+    ) -> hb_bool_t
+{
+    let ffd = &*(font_data as *const FontFuncData);
+
+    let mut ret = FT_Error(0) == FT_Get_Glyph_Name(ffd.ft_face, glyph, name as *mut _, size);
+    if ret && (size != 0 && *name != 0) {
+        ret = false;
+    }
+    ret as hb_bool_t
 }
 
 // type SnftTable = Vec<FT_Byte>;
